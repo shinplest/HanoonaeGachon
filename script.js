c94397b3c55a7aa50951e23530e616282830aaa5
shinplest <shineceo97@naver.com>
2019-12-18 00:16:29 +0900

    add modify button to enable modify feature

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+var modify = false
+
+
+
 $(function () {
-    $("#sortable").sortable();
-    $("#sortable").disableSelection();
+
+    $('#modify').click(function(){
+        $('.test').show();
+        $("#sortable").sortable();
+        $("#sortable").disableSelection();
+        if(modify == false){
+            $("#sortable").sortable();
+            $( "#sortable" ).sortable( "option", "disabled", false ); //다시 바꿀수 있게 하기 위한 코드
+            $("#sortable").disableSelection();
+            $('#modify').html('완료');
+            modify = true;
+        }else{
+            modify = false;
+            $( "#sortable" ).sortable("disable");
+            $('#modify').html('수정');
+        }
+    });
+        $('.basicButtons2').click(function(){
+        $('.test').hide();
+    });
+
 });
