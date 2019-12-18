5ba7489e1ab165b130008e840f6e603455d6c1e6
shinplest <shineceo97@naver.com>
2019-12-18 14:52:19 +0900

    added add and remove feature

--- script.js
+++ script.js
@@ -1,27 +1,94 @@
 var modify = false
 
+function submitItem() {
+    if (!validateItem()) {
+        return;
+    }
+    alert("등록되었습니다.");
+    $('.address').hide();
+}
+
+function validateItem() {
+    var items = $("input[type='text'][name='item']");
+    if (items.length == 0) {
+        alert("작성된 아이템이 없습니다.");
+        return false;
+    }
+
+    var flag = true;
+    for (var i = 0; i < items.length; i++) {
+        if ($(items.get(i)).val().trim() == "") {
+            flag = false;
+            alert("내용을 입력하지 않은 항목이 있습니다.");
+            break;
+        }
+    }
+
+    return flag;
+}
+
+function createBox() {
+    var contents = "<div class='pages'>"
+        + "<img src = 'images/icon.png' class = 'pageicons'>"
+        + "<input type='text' name='item' style='width:200px;' class = 'address'/>"
+        + "</div>";
+    return contents;
+}
+
+function createItem() {
+    $(createBox())
+        .appendTo("#pageBoxWrap")
+        .hover(
+            function () {
+                $(this).css('backgroundColor', '#f9f9f5');
+                $(this).find('.deleteBox').show();
+            },
+            function () {
+                $(this).css('background', 'none');
+                $(this).find('.deleteBox').hide();
+            }
+        )
+        .append("<div class='deleteBox'>[삭제]</div>")
+        .find(".deleteBox").click(function () {
+            var valueCheck = false;
+            $(this).parent().find('input').each(function () {
+                if ($(this).attr("name") != "type" && $(this).val() != '') {
+                    valueCheck = true;
+                }
+            });
+
+            if (valueCheck) {
+                var delCheck = confirm('입력하신 내용이 있습니다.\n삭제하시겠습니까?');
+            }
+            if (!valueCheck || delCheck == true) {
+                $(this).parent().remove();
+                reorder();
+            }
+        });
+}
 
 
 $(function () {
 
-    $('#modify').click(function(){
+    $('#modify').click(function () {
         $('.test').show();
-        $("#sortable").sortable();
-        $("#sortable").disableSelection();
-        if(modify == false){
-            $("#sortable").sortable();
-            $( "#sortable" ).sortable( "option", "disabled", false ); //다시 바꿀수 있게 하기 위한 코드
-            $("#sortable").disableSelection();
+        $("#pageBoxWrap").sortable();
+        $("#pageBoxWrap").disableSelection();
+        if (modify == false) {
+            $("#pageBoxWrap").sortable();
+            $("#pageBoxWrap").sortable("option", "disabled", false); //다시 바꿀수 있게 하기 위한 코드
+            $("#pageBoxWrap").disableSelection();
             $('#modify').html('완료');
             modify = true;
-        }else{
+
+            //이동할수 있는 것을 표현해 주기 위한 애니메이션 코드
+     
+
+        } else {
             modify = false;
-            $( "#sortable" ).sortable("disable");
+            $("#pageBoxWrap").sortable("disable");
             $('#modify').html('수정');
         }
-    });
-        $('.basicButtons2').click(function(){
-        $('.test').hide();
     });
 
 });
