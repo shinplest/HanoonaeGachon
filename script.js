43b119ba26acfa31df16aeda7ef1b0b3be22e10b
shinplest <shineceo97@naver.com>
2019-12-18 19:13:22 +0900

    added add feature

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-var modify = false;
-
-
 //전역변수
+var modify = false; //현재 북마크를 수정할수있는 상태인지 아닌지 판단하는 변수
 
 //입력받은 페이지 이름과 주소를 저장한다. 
 var inputAddress = null;
 var inputName = null;
 
+//페이지들의 배열 생성
 var Pages = new Array();
 
-//페이지를 저장하는 객체 생성자
+//페이지 객체 생성자
 var Page = function(name, address){
     this.name = name;
     this.address = address;
@@ -17,11 +16,12 @@ var Page = function(name, address){
     //this.imgsrc = imgsrc;
 }
 
+
+//메인 jquery 함수
 $(document).ready(function () {
     $('#modify').click(function () {
-        $('.test').show();
-        $("#pageBoxWrap").sortable();
-        $("#pageBoxWrap").disableSelection();
+        
+        //수정이 불가능한 상태에서 수정을 클릭한경우, 드래그 앤 드랍으로 정렬 순서를 바꿀 수 있게 한다.
         if (modify == false) {
             $("#pageBoxWrap").sortable();
             $("#pageBoxWrap").sortable("option", "disabled", false); //다시 바꿀수 있게 하기 위한 코드
@@ -29,17 +29,22 @@ $(document).ready(function () {
             $('#modify').html('완료');
             modify = true;
 
-            //이동할수 있는 것을 표현해 주기 위한 애니메이션 코드
+            //이동할수 있는 것을 표현해 주기 위한 애니메이션 코드 추가 예정
+
+
+        //수정을 완료하면 다시 드래그 앤 드랍 기능을 꺼줌    
         } else {
             modify = false;
             $("#pageBoxWrap").sortable("disable");
             $('#modify').html('수정');
         }
     });
-    //추가 기능 눌렀을때
+
+    //추가 버튼 눌렀을 때
     $('#add').click(function () {
         createItem();
     });
+    //제출 버튼 눌렀을 때
     $('#submit').click(function () {
         submitItem();
     });
@@ -50,9 +55,12 @@ function submitItem() {
         return;
     }
     inputName = $('#nameInput').val();
+
     var inputPage = new Page(inputName, inputAddress);
     Pages.push(inputPage);
-    console.log(Pages[0]);
+    //console.log(Pages[0]);
+    $("#nameInput").hide();
+    $("#newPageName").html(inputName);
     alert("등록되었습니다.");
 }
 
@@ -75,10 +83,17 @@ function validateItem() {
     return flag;
 }
 
+
+
 function createBox() {
-    var contents = "<div class='pages'>"
+    var contents = 
+        "<div class='pages'>"
+        // + "<a href='http://cyber.gachon.ac.kr/' target='_blank'>"
         + "<img src = 'images/icon.png' class = 'pageicons'>"
-        + "<input type='text' id = 'nameInput' name='item' style='width:100px;'/>"
+        + "<input type='text' id = 'nameInput' name='item' style='width:80px;'/>"
+        + "<p id = 'newPageName'>"
+        + "</p>"
+        // + "</a>"
         + "</div>";
     return contents;
 }
