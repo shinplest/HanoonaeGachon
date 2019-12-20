2f0a95030acdfa8a42e460fe5f675bf43c14d950
shinplest <shineceo97@naver.com>
2019-12-20 11:16:00 +0900

    cancle event

--- script.js
+++ script.js
@@ -93,7 +93,9 @@ function createItem() {
     //www.naver.com 올바른 형식으로 입력하지 않은 경우 제외해줌
     //사용자가 http://도 입력한 경우 자동으로 제외해주는 코드 만들기
     inputAddress = prompt("추가할 웹페이지의 주소를 입력하세요.");
+    if(inputAddress == null) return;
     inputName = prompt("추가할 페이지의 이름은?");
+    if(inputName == null) return;
     $(createBox())
         .appendTo("#pageBoxWrap")
         .hover(
